59dc58da88464c4cb267fc162366c75f44a5d3f3
Ernesto Pini <pinie@lens.unifi.it>
2024-04-18 11:36:57 +0200

    add if condition for matched refractive index (speeds up calculation)

--- Rxy_ADE.m
+++ Rxy_ADE.m
@@ -37,12 +37,16 @@ if lx == lz && lx == ly % the isotropic case is treat separately
     Rfun = @(chi) (1/2).*(abs((n.*chi - sqrt(1 - (1 - chi.^2).*n^2))./(n*chi + sqrt(1 - (1 - chi.^2).*n^2))).^2 + abs((chi - n.*sqrt(1 - (1 - chi.^2).*n^2))./(chi + n.*sqrt(1 - (1 - chi.^2).*n^2))).^2);
     Cfun = @(chi) (chi.^2).*Rfun(chi);
     Bfun = @(chi) chi.*Rfun(chi);
-    A = (1 + 3*integral(Cfun, 0, 1))./(1 - 2*integral(Bfun, 0, 1));
 
     Dx = lx*v/3;
     Dy = Dx;
     Dz = Dx;
-    ze = 2*A*lx/3;
+    if n == 1
+        ze = 2*lx/3;
+    else
+        A = (1 + 3*integral(Cfun, 0, 1))./(1 - 2*integral(Bfun, 0, 1));
+        ze = 2*A*lx/3;
+    end
 
 else % anisotropic case
     mux = 1/lx;
@@ -71,9 +75,13 @@ else % anisotropic case
 
     C = integral2(Cfun, 0, 1, 0, 2*pi);
     B = integral2(Bfun, 0, 1, 0, 2*pi);
-    X = integral2(Xfun, 0, 1, 0, 2*pi);
-    Y = integral2(Yfun, 0, 1, 0, 2*pi);
-    ze = (C + Y)/(B - X);
+    if n == 1
+        ze = C/B;
+    else
+        X = integral2(Xfun, 0, 1, 0, 2*pi);
+        Y = integral2(Yfun, 0, 1, 0, 2*pi);
+        ze = (C + Y)/(B - X);
+    end
 end
 
 D = (Dx*Dy*Dz)^(1/3);
